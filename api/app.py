30250b43e4ff3ceae030469bd2103977b70a4e25
gene0524 <yugene524@gmail.com>
2024-03-08 01:14:00 +0000

    style check

--- api/app.py
+++ api/app.py
@@ -1,34 +1,46 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS 
-from database_functions import create_group, display_user_groups, accept_group_invitation,\
-    decline_group_invitation, remove_group, display_group_members, display_top_votes,\
-    display_vote_options, click_vote_dish, cancel_vote, add_food_to_groups
+from flask_cors import CORS
+from database_functions import (
+    create_group,
+    display_user_groups,
+    accept_group_invitation,
+    decline_group_invitation,
+    remove_group,
+    display_group_members,
+    display_top_votes,
+    display_vote_options,
+    click_vote_dish,
+    cancel_vote,
+    add_food_to_groups,
+)
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 ##### user_groups.html #####
 
+
 # Function to create a group
-@app.route('/create-group', methods=['POST'])
+@app.route("/create-group", methods=["POST"])
 def app_create_group():
     try:
         group_data = request.json
         create_group(
-            group_name=group_data.get('groupName'),
-            email_list=group_data.get('groupMembers'),
-            description=group_data.get('groupDetail')
+            group_name=group_data.get("groupName"),
+            email_list=group_data.get("groupMembers"),
+            description=group_data.get("groupDetail"),
         )
-        return jsonify(message='Group data received successfully')
+        return jsonify(message="Group data received successfully")
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Function to display the groups that this user is part of / is invited
-@app.route('/display-user-groups', methods=['POST'])
+@app.route("/display-user-groups", methods=["POST"])
 def app_display_user_groups():
     try:
         request_data = request.json
-        user_email = request_data.get('userEmail')
+        user_email = request_data.get("userEmail")
         groups = display_user_groups(user_email)
         # groups = [
         #     {'group_name': group_name, 'description': description, 'group_id': group_id, 'status': status},
@@ -40,60 +52,65 @@ def app_display_user_groups():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Route to accept a group invitation
-@app.route('/accept-group', methods=['POST'])
+@app.route("/accept-group", methods=["POST"])
 def app_accept_group():
     try:
         request_data = request.json
-        group_id = request_data.get('group_id')
-        email = request_data.get('email')
+        group_id = request_data.get("group_id")
+        email = request_data.get("email")
         result = accept_group_invitation(group_id, email)
         if result:
-            return jsonify(message='Group invitation accepted successfully')
+            return jsonify(message="Group invitation accepted successfully")
         else:
-            return jsonify(error='Failed to accept the group invitation'), 500
+            return jsonify(error="Failed to accept the group invitation"), 500
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Route to decline a group invitation
-@app.route('/decline-group', methods=['POST'])
+@app.route("/decline-group", methods=["POST"])
 def app_decline_group():
     try:
         request_data = request.json
-        group_id = request_data.get('group_id')
-        email = request_data.get('email')
+        group_id = request_data.get("group_id")
+        email = request_data.get("email")
         result = decline_group_invitation(group_id, email)
         if result:
-            return jsonify(message='Group invitation declined successfully')
+            return jsonify(message="Group invitation declined successfully")
         else:
-            return jsonify(error='Failed to decline the group invitation'), 500
+            return jsonify(error="Failed to decline the group invitation"), 500
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Function to remove a group for the member
-@app.route('/remove-group', methods=['POST'])
+@app.route("/remove-group", methods=["POST"])
 def app_remove_group():
     try:
         request_data = request.json
-        group_id = request_data.get('group_id')
-        email = request_data.get('email')
+        group_id = request_data.get("group_id")
+        email = request_data.get("email")
         result = remove_group(group_id, email)
-        
+
         if result:
-            return jsonify(message='Group removed successfully')
+            return jsonify(message="Group removed successfully")
         else:
-            return jsonify(error='Failed to remove group'), 500
+            return jsonify(error="Failed to remove group"), 500
     except Exception as e:
-        return jsonify(error=str(e)), 
+        return (jsonify(error=str(e)),)
+
 
 ##### group_info.html #####
 
+
 # Function to display the group members in a specified group
-@app.route('/display-group-members', methods=['POST'])
+@app.route("/display-group-members", methods=["POST"])
 def app_display_group_members():
     try:
         request_data = request.json
-        group_id = request_data.get('groupId')
+        group_id = request_data.get("groupId")
         members = display_group_members(group_id)
         # members = [
         #     {'email': email, 'first_name': first_name,'last_name': last_name,'stauts': stauts},
@@ -105,12 +122,13 @@ def app_display_group_members():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Function to display the top dish url that has the most votes (LIMIT 5)
-@app.route('/display-top-votes', methods=['POST'])
+@app.route("/display-top-votes", methods=["POST"])
 def app_display_top_votes():
     try:
         request_data = request.json
-        group_id = request_data.get('groupId')
+        group_id = request_data.get("groupId")
         top_votes = display_top_votes(group_id)
         # top_votes = [
         #     {'dish_uri': dish_uri},
@@ -121,15 +139,17 @@ def app_display_top_votes():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 ##### voting.html #####
 
+
 # Function to display the food options to vote (include a flag whether this user has voted for each food)
-@app.route('/display-vote-options', methods=['POST'])
+@app.route("/display-vote-options", methods=["POST"])
 def app_display_vote_options():
     try:
         request_data = request.json
-        group_id = request_data.get('groupId')
-        user_email = request_data.get('userEmail')
+        group_id = request_data.get("groupId")
+        user_email = request_data.get("userEmail")
         dish_options = display_vote_options(group_id, user_email)
         # dishes = [
         #     {'dish_uri': dish_uri, 'votes_count': 3, 'voted_by_user': 0},
@@ -140,47 +160,50 @@ def app_display_vote_options():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 # Function to vote for a dish (check if total votes have exceeded 3)
-@app.route('/click-vote-dish', methods=['POST'])
+@app.route("/click-vote-dish", methods=["POST"])
 def app_click_vote_dish():
     try:
         request_data = request.json
-        group_id = request_data.get('groupId')
-        user_email = request_data.get('userEmail')
-        dish_uri = request_data.get('dishUri')
+        group_id = request_data.get("groupId")
+        user_email = request_data.get("userEmail")
+        dish_uri = request_data.get("dishUri")
         click_vote_dish(group_id, user_email, dish_uri)
-        return jsonify(message='Vote registered successfully')
+        return jsonify(message="Vote registered successfully")
     except Exception as e:
         return jsonify(error=str(e)), 500
-    
+
+
 # Function to cancel the vote for a dish
-@app.route('/cancel-vote-dish', methods=['POST'])
+@app.route("/cancel-vote-dish", methods=["POST"])
 def app_cancel_vote_dish():
     try:
         request_data = request.json
-        group_id = request_data.get('groupId')
-        user_email = request_data.get('userEmail')
-        dish_uri = request_data.get('dishUri')
+        group_id = request_data.get("groupId")
+        user_email = request_data.get("userEmail")
+        dish_uri = request_data.get("dishUri")
         cancel_vote(group_id, user_email, dish_uri)
-        return jsonify(message='Vote registered successfully')
+        return jsonify(message="Vote registered successfully")
     except Exception as e:
         return jsonify(error=str(e)), 500
 
 
 ##### profile.html #####
 
+
 # Function to add a food to Group Food List table
-@app.route('/add-food-to-groups', methods=['POST'])
+@app.route("/add-food-to-groups", methods=["POST"])
 def app_add_food_to_groups():
     try:
         request_data = request.json
-        email = request_data.get('userEmail')
-        dish_uri = request_data.get('dishUri')
+        email = request_data.get("userEmail")
+        dish_uri = request_data.get("dishUri")
         result = add_food_to_groups(email, dish_uri)
         print(result)
         if result:
-            return jsonify(message='Food added successfully')
+            return jsonify(message="Food added successfully")
         else:
-            return jsonify(message='Not a member in any groups!')
+            return jsonify(message="Not a member in any groups!")
     except Exception as e:
         return jsonify(error=str(e)), 500
--- api/database_functions.py
+++ api/database_functions.py
@@ -13,10 +13,11 @@ supabase_client = supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
 
 ##### user_groups.html #####
 
+
 # Function to create a group
 def create_group(group_name, email_list, description):
     group_data = add_group_to_groupbase(group_name, description)
-    group_id = group_data[0]['group_id'] # retrieve group_id
+    group_id = group_data[0]["group_id"]  # retrieve group_id
 
     # Set first person in the email list as the group owner (status 2)
     add_member_to_group(group_id, email_list[0], 2)
@@ -25,105 +26,123 @@ def create_group(group_name, email_list, description):
         add_member_to_group(group_id, email, 0)
     return
 
+
 # subfunction of create_group(), add the group to Group Registration table
 def add_group_to_groupbase(group_name, description):
-    data_to_insert = {
-        "group_name": group_name,
-        "description": description
-    }
-    data, _ = supabase_client.table("Group Registration").insert([data_to_insert]).execute()
+    data_to_insert = {"group_name": group_name, "description": description}
+    data, _ = (
+        supabase_client.table("Group Registration").insert([data_to_insert]).execute()
+    )
     return data[1]
+
 
 # subfunction of create_group(), add the member to Group Members Info table
 def add_member_to_group(group_id, email, status):
-    data_to_insert = {
-        "group_id": group_id,
-        "email": email,
-        "status": status
-    }
+    data_to_insert = {"group_id": group_id, "email": email, "status": status}
     supabase_client.table("Group Members Info").insert([data_to_insert]).execute()
     return
 
+
 # Function to display the groups that this user is part of / is invited
 def display_user_groups(user_email):
-    response, _ = supabase_client.table("Group Members Info")\
-                    .select("group_id, status, Group Registration (group_name, description)")\
-                    .eq("email", user_email)\
-                    .order("group_id")\
-                    .execute()
-    
+    response, _ = (
+        supabase_client.table("Group Members Info")
+        .select("group_id, status, Group Registration (group_name, description)")
+        .eq("email", user_email)
+        .order("group_id")
+        .execute()
+    )
+
     response_list = response[1]
-    
+
     groups = [
         {
-            'group_name': group['Group Registration']['group_name'],
-            'description': group['Group Registration']['description'],
-            'group_id': group['group_id'],
-            'status': group['status']
-        } for group in response_list
+            "group_name": group["Group Registration"]["group_name"],
+            "description": group["Group Registration"]["description"],
+            "group_id": group["group_id"],
+            "status": group["status"],
+        }
+        for group in response_list
     ]
     return groups
 
+
 # Function to accept the group invitation, will set the status to 1 in Group Member Info table
 def accept_group_invitation(group_id, email):
-    data, _ = supabase_client.table("Group Members Info")\
-                    .update({"status": 1})\
-                    .eq("email", email)\
-                    .eq("group_id", group_id)\
-                    .execute()
+    data, _ = (
+        supabase_client.table("Group Members Info")
+        .update({"status": 1})
+        .eq("email", email)
+        .eq("group_id", group_id)
+        .execute()
+    )
     if data[1]:
         print("Successfully accept the group invitation")
         return data[1]
     else:
         print("Error, could not accept the invitation")
 
+
 # Function to decline the group invitation, will delete the row containing email and group id from Group Member Info table
 def decline_group_invitation(group_id, email):
-    data, _ = supabase_client.table("Group Members Info")\
-                .delete()\
-                .eq("group_id", group_id)\
-                .eq("email", email)\
-                .execute()
+    data, _ = (
+        supabase_client.table("Group Members Info")
+        .delete()
+        .eq("group_id", group_id)
+        .eq("email", email)
+        .execute()
+    )
     if data[1]:
         print("Successfully decline the group invitation.")
         return data[1]
     else:
         print("Error, could not decline the invitation")
 
+
 # Function to remove a group for the member
 def remove_group(group_id, email):
     # Check the status of the member in the group
-    response, error = supabase_client.table("Group Members Info")\
-                    .select("status")\
-                    .eq("group_id", group_id)\
-                    .eq("email", email)\
-                    .execute()
+    response, error = (
+        supabase_client.table("Group Members Info")
+        .select("status")
+        .eq("group_id", group_id)
+        .eq("email", email)
+        .execute()
+    )
 
     # If no data found, return early
     if not response[1]:
         print(f"Error fetching member status or member not found!")
         return None
-    
+
     print(response)
-    
+
     # Assuming response is a list of dictionaries
-    member_status = response[1][0]['status']
+    member_status = response[1][0]["status"]
     print(member_status)
 
     # Decide the action based on the member's status
     if member_status == 2:
-        delete_entire_group(group_id) # If user is owner, delete the entire group
+        delete_entire_group(group_id)  # If user is owner, delete the entire group
         return True
     elif member_status == 1:
-        remove_member_from_group(group_id, email) # If user is member, remove the user from the group
+        remove_member_from_group(
+            group_id, email
+        )  # If user is member, remove the user from the group
         return True
     else:
         print("Only the owner and the member could remove the group!")
         return False
 
+
 # subfunction of remove_group(), delete the entire group
 def delete_entire_group(group_id):
-    data, error = supabase_client.table("Group Registration").delete().eq("group_id", group_id).execute()
+    data, error = (
+        supabase_client.table("Group Registration")
+        .delete()
+        .eq("group_id", group_id)
+        .execute()
+    )
     if not data[1]:
         print(f"Error deleting group: {error}")
         return None
@@ -131,9 +150,16 @@ def delete_entire_group(group_id):
         print(f"Group deleted successfully!")
     return
 
+
 # subfunction of remove_group(), delete a member from the group
 def remove_member_from_group(group_id, email):
-    data, error = supabase_client.table("Group Members Info").delete().eq("group_id", group_id).eq("email", email).execute()
+    data, error = (
+        supabase_client.table("Group Members Info")
+        .delete()
+        .eq("group_id", group_id)
+        .eq("email", email)
+        .execute()
+    )
     if not data[1]:
         print(f"Error removing member from group!")
         return None
@@ -141,153 +167,184 @@ def remove_member_from_group(group_id, email):
         print(f"Member successfully removed from group!")
     return
 
+
 ##### group_info.html #####
+
 
 # Function to display the group members in a specified group
 def display_group_members(group_id):
-    response, _ = supabase_client.table("Group Members Info")\
-                        .select("email, status, User Registration (firstname, lastname)")\
-                        .eq("group_id", group_id)\
-                        .order("status", desc=True)\
-                        .execute()
+    response, _ = (
+        supabase_client.table("Group Members Info")
+        .select("email, status, User Registration (firstname, lastname)")
+        .eq("group_id", group_id)
+        .order("status", desc=True)
+        .execute()
+    )
     response_list = response[1]
     members = [
         {
-            'email': member['email'],
-            'first_name': member['User Registration']['firstname'],
-            'last_name': member['User Registration']['lastname'],
-            'status': member['status']
-        } for member in response_list
+            "email": member["email"],
+            "first_name": member["User Registration"]["firstname"],
+            "last_name": member["User Registration"]["lastname"],
+            "status": member["status"],
+        }
+        for member in response_list
     ]
     print(members)
     return members
 
+
 # Function to display the top dish url that has the most votes (LIMIT 5)
 def display_top_votes(group_id):
-    voted_food, _ = supabase_client.table("Group Food List")\
-                    .select("dish_uri")\
-                    .eq("group_id", group_id)\
-                    .gt("votes_count", 0)\
-                    .order("votes_count", desc=True)\
-                    .limit(5)\
-                    .execute()
+    voted_food, _ = (
+        supabase_client.table("Group Food List")
+        .select("dish_uri")
+        .eq("group_id", group_id)
+        .gt("votes_count", 0)
+        .order("votes_count", desc=True)
+        .limit(5)
+        .execute()
+    )
     voted_food_list = voted_food[1]
     return voted_food_list
 
+
 ##### voting.html #####
+
 
 # Function to display the food options to vote (include a flag whether this user has voted for each food)
 def display_vote_options(group_id, user_email):
-    response_1, _ = supabase_client.table("Group Food List")\
-                    .select("dish_uri, votes_count")\
-                    .eq("group_id", group_id)\
-                    .execute()
-    
+    response_1, _ = (
+        supabase_client.table("Group Food List")
+        .select("dish_uri, votes_count")
+        .eq("group_id", group_id)
+        .execute()
+    )
+
     dish_list = response_1[1]
-    
-    response_2, _ = supabase_client.table("Group Vote")\
-                    .select("dish_uri")\
-                    .eq("group_id", group_id).eq("email", user_email)\
-                    .execute()
+
+    response_2, _ = (
+        supabase_client.table("Group Vote")
+        .select("dish_uri")
+        .eq("group_id", group_id)
+        .eq("email", user_email)
+        .execute()
+    )
 
     options_voted = response_2[1]
-    
+
     # Create a list of dish_uris that the user has voted for
-    voted_list = [vote['dish_uri'] for vote in options_voted]  
-    
+    voted_list = [vote["dish_uri"] for vote in options_voted]
+
     dishes = [
         {
-            'dish_uri': dish['dish_uri'],
-            'votes_count': dish['votes_count'],
-            'voted_by_user': dish['dish_uri'] in voted_list
-        } for dish in dish_list
+            "dish_uri": dish["dish_uri"],
+            "votes_count": dish["votes_count"],
+            "voted_by_user": dish["dish_uri"] in voted_list,
+        }
+        for dish in dish_list
     ]
     return dishes
+
 
 # Function to vote for a dish (check if total votes have exceeded 3)
 def click_vote_dish(group_id, user_email, dish_uri):
     valid_click = False
-    
+
     # Give the count of how many times this user has voted
-    num_voted, _ = supabase_client.table("Group Vote")\
-                    .select('*')\
-                    .eq("group_id", group_id)\
-                    .eq("email", user_email)\
-                    .execute()
+    num_voted, _ = (
+        supabase_client.table("Group Vote")
+        .select("*")
+        .eq("group_id", group_id)
+        .eq("email", user_email)
+        .execute()
+    )
     count = len(num_voted[1])
-    
+
     # Validate if the user has the right to vote (owner)
     if count < 3:
         valid_click = True
-                
+
     # If valid to vote, add the dish uri to the table
     if valid_click:
         data_to_insert = {
-            'group_id': group_id,
-            'dish_uri': dish_uri,
-            'email': user_email
+            "group_id": group_id,
+            "dish_uri": dish_uri,
+            "email": user_email,
         }
         # Insert the data into Group Vote
-        data, _ = supabase_client.table("Group Vote")\
-                    .insert([data_to_insert])\
-                    .execute()
-        
+        data, _ = supabase_client.table("Group Vote").insert([data_to_insert]).execute()
+
         # Get the updated count
-        new_count, _ = supabase_client.table("Group Vote")\
-                    .select('*')\
-                    .eq("group_id", group_id)\
-                    .eq("dish_uri", dish_uri)\
-                    .execute()
+        new_count, _ = (
+            supabase_client.table("Group Vote")
+            .select("*")
+            .eq("group_id", group_id)
+            .eq("dish_uri", dish_uri)
+            .execute()
+        )
         new_count = len(new_count[1])
-        
+
         # Update the vote count in the Group Food List table
-        data, _ = supabase_client.table("Group Food List")\
-                    .update({"votes_count": new_count})\
-                    .eq("dish_uri", dish_uri)\
-                    .eq("group_id", group_id)\
-                    .execute()
+        data, _ = (
+            supabase_client.table("Group Food List")
+            .update({"votes_count": new_count})
+            .eq("dish_uri", dish_uri)
+            .eq("group_id", group_id)
+            .execute()
+        )
         print("Successfully voted")
     else:
         print("Vote is not successful. Check your condition")
-        
+
+
 def cancel_vote(group_id, user_email, dish_uri):
-    data, _ = supabase_client.table("Group Vote")\
-                .delete()\
-                .eq("group_id", group_id)\
-                .eq("email", user_email)\
-                .eq("dish_uri", dish_uri)\
-                .execute()
+    data, _ = (
+        supabase_client.table("Group Vote")
+        .delete()
+        .eq("group_id", group_id)
+        .eq("email", user_email)
+        .eq("dish_uri", dish_uri)
+        .execute()
+    )
     # Get the updated count
-    new_count, _ = supabase_client.table("Group Vote")\
-                    .select('*')\
-                    .eq("group_id", group_id)\
-                    .eq("dish_uri", dish_uri)\
-                    .execute()
+    new_count, _ = (
+        supabase_client.table("Group Vote")
+        .select("*")
+        .eq("group_id", group_id)
+        .eq("dish_uri", dish_uri)
+        .execute()
+    )
     new_count = len(new_count[1])
-        
+
     # Update the vote count in the Group Food List table
-    data, _ = supabase_client.table("Group Food List")\
-                    .update({"votes_count": new_count})\
-                    .eq("dish_uri", dish_uri)\
-                    .eq("group_id", group_id)\
-                    .execute()
+    data, _ = (
+        supabase_client.table("Group Food List")
+        .update({"votes_count": new_count})
+        .eq("dish_uri", dish_uri)
+        .eq("group_id", group_id)
+        .execute()
+    )
     if data[1]:
         print("Successfully cancel the vote.")
         return data[1]
     else:
         print("Error, could not cancel the vote.")
-     
+
 
 ##### profile.html #####
+
 
 # Function to add a food to Group Food List table
 def add_food_to_groups(email, dish_uri):
     # Query the "Group Members Info" to get the group_ids for the given email
-    data, error = supabase_client.table("Group Members Info")\
-        .select("group_id")\
-        .eq("email", email)\
-        .neq("status", 0)\
+    data, error = (
+        supabase_client.table("Group Members Info")
+        .select("group_id")
+        .eq("email", email)
+        .neq("status", 0)
         .execute()
+    )
 
     group_id_data = data[1]
 
@@ -297,7 +354,7 @@ def add_food_to_groups(email, dish_uri):
         return False
 
     # # Extract the group_ids from the query result
-    group_ids = [item['group_id'] for item in group_id_data]
+    group_ids = [item["group_id"] for item in group_id_data]
 
     # For each group_id, add the dish_uri to the "Group Food List"
     for group_id in group_ids:
@@ -309,15 +366,14 @@ def add_food_to_groups(email, dish_uri):
             print(f"Failed to add dish to group {group_id}.")
     return True
 
+
 # subfunction of add_food_to_groups(), add the dish_uri to Group Food List table
 def add_food_to_food_list(group_id, dish_uri):
     # Assume the implementation of this function is as provided earlier
-    data_to_insert = {
-        "group_id": group_id,
-        "dish_uri": dish_uri,
-        "votes_count": 0
-    }
-    data, error = supabase_client.table("Group Food List").insert([data_to_insert]).execute()
+    data_to_insert = {"group_id": group_id, "dish_uri": dish_uri, "votes_count": 0}
+    data, error = (
+        supabase_client.table("Group Food List").insert([data_to_insert]).execute()
+    )
     if not data:
         print(f"Error inserting data!")
         return False
